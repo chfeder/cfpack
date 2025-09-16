a51ab6b9961975c1059c6d5cff316628567a9530
christoph.federrath@anu.edu.au <christoph.federrath@anu.edu.au>
2025-09-16 17:05:15 +1000

    cfpack version 0.4.8

--- pypi/setup.py
+++ pypi/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cfpack",
-    version="0.4.7",
+    version="0.4.8",
     author="Christoph Federrath",
     author_email="christoph.federrath@anu.edu.au",
     description="Christoph Federrath (CF) python package (cfpack)",
