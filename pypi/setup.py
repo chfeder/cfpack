43ad47e5a5883614d247610f7c7ff4ed7a286cf9
Christoph Federrath <chfeder@TwisterMini.local>
2025-09-16 12:19:24 +1000

    cfpack version 0.4.7

--- pypi/setup.py
+++ pypi/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cfpack",
-    version="0.4.6",
+    version="0.4.7",
     author="Christoph Federrath",
     author_email="christoph.federrath@anu.edu.au",
     description="Christoph Federrath (CF) python package (cfpack)",
